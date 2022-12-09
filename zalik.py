65a550f4978f8a7d781b3871663c59bc272b6132
soloveyOlexandr <120163436+soloveyOlexandr@users.noreply.github.com>
2022-12-09 09:57:30 +0200

    Delete zalik.py

--- zalik.py
+++
@@ -1,5 +0,0 @@
-with open('file.txt', encoding='utf-8') as file:
-    for line in file:
-        print(f'рядок: {line}')
-        print(f'кількість символів: {len(line)}')
-        print(f'кількість слів: {len(line.split())}')
